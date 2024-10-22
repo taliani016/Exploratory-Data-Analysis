0e62c4996504f6a33456687ac4d98c801496a352
taliani016 <reda.lahlimi2001@gmail.com>
2024-10-22 11:51:01 +0100

    plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,18 @@
-# Load common setup
-source("setup.R")
+# Load necessary packages
+library(data.table)
 
-# Open PNG device
+# Read the data
+data <- read.table(file.choose(), header = TRUE, sep = ";", na.strings = "?")
+
+# Convert the Date and Time variables to Date/Time classes
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Time <- strptime(data$Time, format = "%H:%M:%S")
+
+# Subset the data for the dates 2007-02-01 and 2007-02-02
+data_subset <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+# Create the histogram
 png("plot1.png", width = 480, height = 480)
+hist(data_subset$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
-# Create histogram for Global Active Power
-hist(data_filtered$Global_active_power, col = "red", 
-     main = "Global Active Power", 
-     xlab = "Global Active Power (kilowatts)", 
-     ylab = "Frequency")
-
-# Close the PNG file
 dev.off()
